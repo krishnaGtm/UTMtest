d10a60dad4437feabad9d8449b9ca61f6d79750a
kgautam <kgautam@KRISHNAG-JV>
2020-07-28 18:03:38 +0545

    #13344:Change on stored procedure.

--- Enza.UTM.DbUp/Scripts/000358.13344.PR_RDT_GetTestOverview.sql
+++ Enza.UTM.DbUp/Scripts/000358.13344.PR_RDT_GetTestOverview.sql
@@ -1,55 +1,82 @@
-DROP PROCEDURE IF EXISTS [dbo].[PR_RDT_GetMaterialForUpload]
-GO
+/*
+=========Changes====================
+Changed By			DATE				Description
 
+Krishna Gautam		2020-07-15			#11251: Calculation of total plates used for DNA marker issue fixed.	
 
---EXEC PR_RDT_GetMaterialForUpload 2064;
-CREATE PROCEDURE [dbo].[PR_RDT_GetMaterialForUpload]
+========Example=============
+EXEC PR_RDT_GetTestOverview null, 'ON,TO','','',1,211
+*/
+
+ALTER PROCEDURE [dbo].[PR_RDT_GetTestOverview]
 (
-	@TestID		INT
-) AS BEGIN
-	SET NOCOUNT ON;
+    @Active	BIT  = NULL,
+	@Crops NVARCHAR(MAX),
+	@Filter NVARCHAR(MAX),
+	@Sort NVARCHAR(MAX),
+	@Page INT,
+	@PageSize INT
+)
+AS BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @Query NVARCHAR(MAX), @Offset INT,@CropCodes NVARCHAR(MAX);
+
+    CREATE TABLE #Status(StatusCode INT, StatusName NVARCHAR(50));
+
+    INSERT #Status(StatusCode, StatusName)
+    SELECT StatusCode, StatusName
+    FROM [Status]
+    WHERE StatusTable = 'Test';
+    IF(@Active IS NOT NULL AND @Active = 0) BEGIN
+	   DELETE #Status WHERE StatusCode <> 700;
+    END
+    ELSE IF (@Active IS NOT NULL AND @Active = 1) BEGIN
+	   DELETE #Status WHERE StatusCode = 700;	   
+    END
+
+    SELECT @CropCodes = COALESCE(@CropCodes + ',', '') + ''''+ T.[value] +'''' FROM 
+	   string_split(@Crops,',') T
+
+    SET @Offset = @PageSize * (@Page -1);
+
+    IF(ISNULL(@Filter,'')<> '') BEGIN
+	   SET @Filter = 'WHERE '+@Filter
+    END
+    ELSE
+	   SET @Filter = '';
+
+	SET @Query =	N';WITH CTE AS 
+					(
+						SELECT * FROM 
+						(
+							SELECT T.TestID, T.TestTypeID, Crop = F.CropCode, [BreedingStation] = T.BreedingStationCode ,[Test] = T.TestName,
+							T.PlannedDate, T1.ExpectedDate, [Status] = Stat.StatusName, 
+							T1.UsedMarkers,
+							T.StatusCode
+							FROM Test T
+							JOIN [File] F ON F.FileID = T.FileID
+							JOIN #Status Stat ON Stat.StatusCode = T.StatusCode
+							JOIN
+							(
+								SELECT 
+									TestID, 
+									ExpectedDate = MAX(ExpectedDate),
+									UsedMarkers = COUNT(DISTINCT DeterminationID)
+								FROM TestMaterialDetermination
+								GROUP BY TestID
+							) T1 ON T.TestID = T1.TestID
+							WHERE T.TestTypeID = 8 AND F.CropCode IN  ('+@CropCodes +') AND T.CreationDate >= DATEADD(YEAR,-1,GetDate())
+						) T1 '+@Filter+'
+					), COUNT_CTE AS (SELECT COUNT(TestID) AS TotalRows FROM CTE)
+					SELECT CTE.*,
+					Count_CTE.[TotalRows] FROM CTE,COUNT_CTE
+					ORDER BY CTE.PlannedDate DESC
+					OFFSET '+CAST(@offset AS varchar(MAX))+' ROWS
+					FETCH NEXT '+CAST (@pageSize AS VARCHAR(MAX))+' ROWS ONLY
+					--OPTION (USE HINT ( ''FORCE_LEGACY_CARDINALITY_ESTIMATION'' ))
+					'
 	
-	IF NOT EXISTS(SELECT TestID FROM Test WHERE TestID = @TestID)
-	BEGIN
-		EXEC PR_ThrowError N'Invalid test.';
-		RETURN;
-	END
-
-	IF NOT EXISTS(SELECT TestID FROM Test WHERE TestID = @TestID AND StatusCode = 100)
-	BEGIN
-		EXEC PR_ThrowError N'Invalid test status.';
-		RETURN;
-	END
-
-	IF EXISTS(SELECT TestID FROM TestMaterialDetermination WHERE TestID = @TestID AND ISNULL(ExpectedDate,'') = '')
-	BEGIN
-		EXEC PR_ThrowError N'Expected result date is not filled for all materials.';
-		RETURN;
-	END
-
-	SELECT
-		F.CropCode AS 'Crop',
-		T.BreedingStationCode AS 'BrStation',
-		T.CountryCode AS 'Country',
-		T.ImportLevel AS 'Level',
-		TT.TestTypeCode AS 'TestType',
-		T.TestID AS 'RequestID',
-		'UTM' AS 'RequestingSystem',
-		TMD.DeterminationID,
-		M.MaterialID,
-		M.MaterialKey,
-		CONVERT(varchar(50), TMD.ExpectedDate, 120) AS 'ExpectedResultDate',
-		TM.MaterialStatus
-	FROM Test T
-	JOIN TestType TT ON TT.TestTypeID = T.TestTypeID
-	JOIN [File] F ON F.FileID = T.FileID
-	JOIN [Row] R ON R.FileID = F.FileID
-	JOIN Material M ON M.MaterialKey = R.MaterialKey
-	JOIN TestMaterialDetermination TMD ON TMD.TestID = T.TestID AND TMD.MaterialID = M.MaterialID
-	LEFT JOIN TestMaterial TM ON TM.TestID = T.TestID AND TM.MaterialID = M.MaterialID
-	WHERE T.TestID = @TestID
+	EXEC sp_executesql @Query;
 
 END
-GO
-
-
