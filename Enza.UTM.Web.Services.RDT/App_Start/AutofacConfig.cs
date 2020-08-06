68aa42c5b6c16a7ac1aa0df953d7101db381f270
binod.gurung@javra.com <binod.gurung@javra.com>
2020-08-06 15:17:21 +0545

    #15265 : New project for RDT services added in the solution.

---
+++ Enza.UTM.Web.Services.RDT/App_Start/AutofacConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Enza.UTM.BusinessAccess;
+
+namespace Enza.UTM.Web.Services.RDT
+{
+    public static class AutofacConfig
+    {
+        public static void Configure(HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            //register other types
+            builder.RegisterModule<AutofacModule>();
+
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
