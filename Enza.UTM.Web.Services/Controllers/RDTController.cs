99b030e1053e181d62734ca3f72e84f27bc7db66
Dibya Mani Suvedi <suvedi2003@gmail.com>
2020-09-23 16:05:17 +0545

    moved callback service of RDTmodule to utm services

--- Enza.UTM.Web.Services/Controllers/RDTController.cs
+++ Enza.UTM.Web.Services/Controllers/RDTController.cs
@@ -167,5 +167,29 @@ namespace Enza.UTM.Web.Services.Controllers
             var rs = await _rdtService.GetMappingColumnsAsync();
             return Ok(rs);
         }
+
+        [HttpPost]
+        [Route("RequestSampleTestCallBack")]
+        //[Authorize(Roles = AppRoles.HANDLE_LAB_CAPACITY + "," + AppRoles.REQUEST_TEST)]
+        public async Task<IHttpActionResult> RequestSampleTestCallBack([FromBody] RequestSampleTestCallBackRequestArgs requestArgs)
+        {
+            if (requestArgs == null)
+                return InvalidRequest("Please provide required parameters.");
+
+            var result = await _rdtService.RequestSampleTestCallbackAsync(requestArgs);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("ReceiveRDTResults")]
+        //[Authorize(Roles = AppRoles.HANDLE_LAB_CAPACITY + "," + AppRoles.REQUEST_TEST)]
+        public async Task<IHttpActionResult> ReceiveRDTResults([FromBody] ReceiveRDTResultsRequestArgs requestArgs)
+        {
+            if (requestArgs == null)
+                return InvalidRequest("Please provide required parameters.");
+
+            var result = await _rdtService.ReceiveRDTResultsAsync(requestArgs);
+            return Ok(result);
+        }
     }
 }
