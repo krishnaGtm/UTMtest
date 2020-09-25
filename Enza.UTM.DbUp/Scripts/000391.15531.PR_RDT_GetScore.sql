eff6fb82728b111c4477f345a2eeeeb7f91e9a75
kgautam <kgautam@KRISHNAG-JV>
2020-09-25 11:24:17 +0545

    #15531: Changes on stored procedure to address flow 2 for sending RDT result.

---
+++ Enza.UTM.DbUp/Scripts/000391.15531.PR_RDT_GetScore.sql
@@ -0,0 +1,131 @@
+
+/*
+=========Changes====================
+Changed By			Date				Description
+
+Krishna Gautam		2020-08-10			#15150: Created Stored Procedure	
+
+========Example=============
+EXEC PR_RDT_GetScore 10628
+
+*/
+
+
+ALTER PROCEDURE [dbo].[PR_RDT_GetScore]
+(
+	@TestID INT
+)
+AS 
+BEGIN
+	DECLARE @CropCode NVARCHAR(MAX);
+	DECLARE @FlowType INT;
+	
+
+	SELECT @CropCode = CropCode, @FlowType = TestFlowType
+	FROM [File] F 
+	JOIN Test T ON T.FileID = F.FileID
+	WHERE T.TestID = @TestID;
+
+	IF(@FlowType = 1)
+	BEGIN
+		SELECT 
+			T.TestID, 
+			M.MaterialKey, 
+			M.RefExternal, 
+			T1.ColumnLabel, 
+			Score = CASE WHEN ISNULL(TDR.TraitResult,'') <> '' THEN TDR.TraitResult ELSE TR.Score END, 
+			TM.PhenomeObsID,
+			T.ImportLevel, 
+			M.MaterialID,
+			TR.RDTTestResultID,
+			TR.ResultStatus,
+			TDR.RDTTraitDetResultID,
+			TDR.TraitResult,
+			FlowType = @FlowType		
+		FROM Test T
+		JOIN TestMaterial TM ON TM.TestID = T.TestID
+		JOIN Material M ON M.MaterialID = TM.MaterialID
+		JOIN RDTTestResult TR ON TR.TestID = T.TestID AND M.MaterialID = TR.MaterialID
+		JOIN RelationTraitDetermination RTD ON RTD.DeterminationID = TR.DeterminationID
+		JOIN CropTrait CT ON CT.CropTraitID = RTD.CropTraitID AND CT.CropCode = @CropCode
+		JOIN Trait T1 ON T1.TraitID = CT.TraitID
+		JOIN 
+		(
+			SELECT C.TraitID FROM [Column] C
+			JOIN [File] F ON F.FileID = C.FileID
+			JOIN [Test] T ON T.FileID = F.FileID
+			WHERE T.TestID = @TestID AND ISNULL(C.TraitID,0) > 0
+		) C1 ON C1.TraitID = T1.TraitID
+		LEFT JOIN RDTTraitDetResult TDR ON TDR.RelationID = RTD.RelationID AND ISNULL(TDR.DetResult,'') = ISNULL(TR.Score,'')
+		WHERE T.TestID = @TestID AND TR.ResultStatus IN (100,200)
+		order by MaterialID
+	END
+	ELSE IF (@FlowType = 2)
+	BEGIN
+		SELECT 
+			T.TestID, 
+			M.MaterialKey, 
+			M.RefExternal, 
+			T1.ColumnLabel, 
+			Score = CASE WHEN ISNULL(TDR.TraitResult,'') <> '' THEN TDR.TraitResult ELSE TR.Score END, 
+			TM.PhenomeObsID,
+			T.ImportLevel, 
+			M.MaterialID,
+			TR.RDTTestResultID,
+			TR.ResultStatus,
+			TDR.RDTTraitDetResultID,
+			TDR.TraitResult,
+			FlowType = @FlowType
+		FROM Test T
+		JOIN TestMaterial TM ON TM.TestID = T.TestID
+		JOIN Material M ON M.MaterialID = TM.MaterialID
+		JOIN RDTTestResult TR ON TR.TestID = T.TestID AND M.MaterialID = TR.MaterialID
+		LEFT JOIN RelationTraitDetermination RTD ON RTD.DeterminationID = TR.DeterminationID
+		LEFT JOIN CropTrait CT ON CT.CropTraitID = RTD.CropTraitID AND CT.CropCode = @CropCode
+		LEFT JOIN Trait T1 ON T1.TraitID = CT.TraitID
+		LEFT JOIN RDTTraitDetResult TDR ON 
+					TDR.RelationID = RTD.RelationID
+					AND (
+							(TR.SusceptibilityPercent BETWEEN TDR.MinPercent AND TDR.MaxPercent)
+							OR
+							(ISNULL(TR.Score,'') <> '' AND (ISNULL(TDR.MinPercent,0) = 0 OR ISNULL(TDR.MaxPercent,0) = 0))
+						)
+					AND CASE WHEN ISNULL(TM.MaterialStatus,'') = ISNULL(TDR.MaterialStatus,'') THEN 1
+							WHEN ISNULL(TDR.MaterialStatus,'') = '' THEN 1
+							ELSE 0
+							END = 1
+		WHERE T.TestID = @TestID AND TR.ResultStatus IN (100,200)
+		order by MaterialID, TDR.MaterialStatus DESC
+		--This order by is used in executable and it will select first data and ignore second data if same observationID is found.
+	END
+	ELSE IF (@FlowType = 3)
+	BEGIN
+		SELECT 
+			T.TestID, 
+			M.MaterialKey, 
+			M.RefExternal, 
+			T1.ColumnLabel, 
+			TR.Score, 
+			TM.PhenomeObsID,
+			T.ImportLevel, 
+			M.MaterialID,
+			TR.RDTTestResultID,
+			TR.ResultStatus,
+			TDR.RDTTraitDetResultID,
+			TDR.TraitResult,
+			FlowType = @FlowType
+		FROM Test T
+		JOIN TestMaterial TM ON TM.TestID = T.TestID
+		JOIN Material M ON M.MaterialID = TM.MaterialID
+		JOIN RDTTestResult TR ON TR.TestID = T.TestID AND M.MaterialID = TR.MaterialID
+		LEFT JOIN RelationTraitDetermination RTD ON RTD.DeterminationID = TR.DeterminationID
+		LEFT JOIN CropTrait CT ON CT.CropTraitID = RTD.CropTraitID AND CT.CropCode = @CropCode
+		LEFT JOIN Trait T1 ON T1.TraitID = CT.TraitID		
+		LEFT JOIN RDTTraitDetResult TDR ON 
+					TDR.RelationID = RTD.RelationID
+					AND ISNULL(TDR.MappingCol,'') = ISNULL(TR.MappingColumn,'') 
+		WHERE T.TestID = @TestID AND TR.ResultStatus IN (100,200)
+		order by MaterialID
+	END
+
+END
