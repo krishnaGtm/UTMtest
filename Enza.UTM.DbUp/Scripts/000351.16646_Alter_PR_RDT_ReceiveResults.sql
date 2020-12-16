99d8a7b8f90142f2b6ef6270ad5d84a166bd85f6
binod.gurung@javra.com <binod.gurung@javra.com>
2020-12-16 12:55:39 +0545

    Dbup error fix 7 for acceptance release.

--- Enza.UTM.DbUp/Scripts/000351.16646_Alter_PR_RDT_ReceiveResults.sql
+++ Enza.UTM.DbUp/Scripts/000351.16646_Alter_PR_RDT_ReceiveResults.sql
@@ -1,16 +1,14 @@
-DROP PROCEDURE IF EXISTS [dbo].[PR_RDT_ReceiveResults]
-GO
+IF TYPE_ID(N'TVP_RDTScore') IS NULL
+BEGIN
 
-DROP TYPE IF EXISTS [dbo].[TVP_RDTScore]
-GO
+	CREATE TYPE [dbo].[TVP_RDTScore] AS TABLE(
+		[DeterminationID] [int] NULL,
+		[MaterialID] [int] NULL,
+		[Score] [nvarchar](255) NULL
+	)
 
-CREATE TYPE [dbo].[TVP_RDTScore] AS TABLE(
-	[OriginID] [int] NULL,
-	[MaterialID] [int] NULL,
-	[Score] [nvarchar](255) NULL
-)
+END
 GO
-
 
 IF NOT EXISTS(SELECT 1 FROM sys.columns 
           WHERE Name = N'Source'
@@ -33,43 +31,6 @@ BEGIN
 	ADD OriginID INT
 
 END
-GO
-
-
-CREATE PROCEDURE [dbo].[PR_RDT_ReceiveResults]
-(
-	@TestID INT,
-	@TestFlowType INT,
-	@TVP_RDTScore TVP_RDTScore READONLY
-) AS
-
-BEGIN
-SET NOCOUNT ON;
-	BEGIN TRY
-		BEGIN TRANSACTION;
-					
-			INSERT INTO RDTTestResult(TestID, DeterminationID, MaterialID, Score, ResultStatus, SusceptibilityPercent, MappingColumn)
-			SELECT @TestID, D.DeterminationID, T1.MaterialID, T1.Score, 100, SusceptibilityPercent, ValueColumn	
-			FROM @TVP_RDTScore T1
-			JOIN Determination D ON D.OriginID = T1.OriginID AND D.Source = 'StarLims'
-			JOIN TestMaterialDetermination TMD ON TMD.DeterminationID = D.DeterminationID AND TMD.MaterialID = T1.MaterialID
-			WHERE TMD.TestID = @TestID
-			GROUP BY D.DeterminationID, T1.MaterialID, T1.Score, T1.SusceptibilityPercent, T1.ValueColumn;
-
-			UPDATE Test 
-				SET StatusCode = 550, --Partially Received
-					TestFlowType = @TestFlowType
-			WHERE TestID = @TestID;
-			
-		COMMIT;
-	END TRY
-	BEGIN CATCH
-		IF @@TRANCOUNT > 0
-            ROLLBACK;
-		THROW;
-	END CATCH
-END
-
 GO
 
 
